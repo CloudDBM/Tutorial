9f6ae19b72900813a05b8775e771840d0f31c131
Cloud DB <44365807+CloudDBM@users.noreply.github.com>
2023-03-29 11:29:49 -0300

    29-03-2023

---
+++ clean-logs.ps1
@@ -0,0 +1,5 @@
+﻿$Path = "C:\Temp"
+$Daysback = "-5"
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object { $_.Name -like '*.log' } | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
